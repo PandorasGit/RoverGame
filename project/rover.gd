a2b3281c95e95fcab83fa7e06ce9f00b13f64f08
Victoria Moon <vanastasiamoon@gmail.com>
2023-03-28 17:21:15 -0400

    Add cam and spin functionality

--- project/rover.gd
+++ project/rover.gd
@@ -1,20 +1,44 @@
 extends Node3D
 
 
+@onready var rover = $VehicleBody3D
+@onready var frontRightWheel = $VehicleBody3D/FrontRight
+@onready var frontLeftWheel = $VehicleBody3D/FrontLeft
+@onready var middleRightWheel = $VehicleBody3D/MiddleRight
+@onready var middleLeftWheel = $VehicleBody3D/MiddleLeft
+@onready var backRightWheel = $VehicleBody3D/BackRight
+@onready var backLeftWheel = $VehicleBody3D/BackLeft
+var current_cam := 0
+
 func forward():
-	$VehicleBody3D.brake = 0
-	$VehicleBody3D.engine_force = -10
+	rover.brake = 0
+	rover.engine_force = -20
 	
 	
 func reverse():
-	$VehicleBody3D.brake = 0
-	$VehicleBody3D.engine_force = 10
+	rover.brake = 0
+	rover.engine_force = 20
 	
 	
 func brake():
-	$VehicleBody3D.engine_force = 0
-	$VehicleBody3D.brake = 0.5
+	rover.engine_force = 0
+	rover.brake = 0.5
 	
 	
 func spin():
-	pass
+	rover.engine_force = 0
+	frontRightWheel.engine_force = 100
+	middleRightWheel.engine_force = 100
+	backRightWheel.engine_force = 100
+	frontLeftWheel.engine_force = -100
+	middleLeftWheel.engine_force = -100
+	backLeftWheel.engine_force = -100
+	
+	
+func change_camera():
+	var cams = $VehicleBody3D/Node.get_children()
+	current_cam += 1
+	if current_cam > 2:
+		current_cam = 0
+	cams[current_cam].current = true
+	print(cams[current_cam].name)
