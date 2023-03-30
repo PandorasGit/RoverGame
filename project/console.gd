9a379c5bdafe5d28fbad6fd9e70c6a9a6d34c193
Victoria Moon <vanastasiamoon@gmail.com>
2023-03-30 19:51:17 -0400

    Restrict player interaction with terminal input

--- project/console.gd
+++ project/console.gd
@@ -1,41 +1,26 @@
-extends TextEdit
+extends Control
+
+@onready var command_history = $History
+@onready var terminal = $Terminal
+var terminal_text
 
 
-
-var command := [0, 0, 0, 0, 0]
-
-
-func _on_text_changed():
-	if text.ends_with("\n"):
-		_parse()
+func _on_terminal_text_submitted(new_text):
+	command_history.text += terminal.text + "\n"
+	parse_terminal()
+	terminal.clear()
 
 
-func _parse():
-	var current_line = get_line(get_caret_line() - 1)
-	var command_array = current_line.split(",")
-	for commands in command_array:
-		
-		if commands=="forward":
-			command[0] = 1
-			print("SiS")
-		if commands == "reverse":
-			print("SOS")
-		if commands == "brake":
-			print("SUS")
-		if commands == "spin":
-			print("SAS")
-
-
-func _backspace(line_number):
-	var current_line = get_caret_line()
-	var current_line_content = get_line(current_line)
-
-	var current_column = get_caret_column()
-	
-	if current_line_content.length() > 0:
-		text = text.rstrip(text[text.length()-1])
-		set_caret_column(current_column)
-		set_caret_line(current_line)
-		print(text)
-
-
+func parse_terminal():
+	terminal_text = terminal.text.split(",")
+	for command in terminal_text:
+		if command == "forward":
+			print("forward")
+		if command == "reverse":
+			print("reverse")
+		if command == "brake":
+			print("brake")
+		if command == "spin":
+			print("spin")
+		if command == "camera":
+			print("camera")
