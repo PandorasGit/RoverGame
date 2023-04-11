9b1bb80e63b5adc42f07194c7f90aeafc78f8784
Victoria Moon <vanastasiamoon@gmail.com>
2023-04-11 17:39:10 -0400

    Readd wheels

--- project/Rover/rover.gd
+++ project/Rover/rover.gd
@@ -46,7 +46,7 @@ func spin():
 func change_camera():
 	var cams = $VehicleBody3D/Cameras.get_children()
 	current_cam += 1
-	if current_cam > 2:
+	if current_cam > 3:
 		current_cam = 0
 	cams[current_cam].current = true
 	print(cams[current_cam].name)
